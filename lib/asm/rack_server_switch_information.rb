de81d8bdeb3d908fb4e00e7bd6386befce4444ed
Sushil_R <Sushil_R@WN7X64-31Y7QV1.delds.amer.dell.com>
2014-01-31 10:20:38 +0530

    ToR Configuration for M420 changes ToR Configuration for powerconnect switch

--- lib/asm/rack_server_switch_information.rb
+++ lib/asm/rack_server_switch_information.rb
@@ -21,7 +21,7 @@ class Rack_server_switch_information
       output = `sudo puppet facts find #{certname} --terminus yaml --clientyamldir=/var/opt/lib/pe-puppet/yaml/ --color=false`
       logger.debug "************ output :: #{output} ******************"
       if output.empty? || ( !output.match("remotedeviceinfo") && !output.match("remote_device_info") )
-      next
+        next
       end
       hash = Crack::JSON.parse(output)
       valuesHash=hash["values"]
@@ -42,55 +42,146 @@ class Rack_server_switch_information
   end
 end
 
+def search_server_Macaddress_force10(certname,mac,logger)
+  interface_found="false"
+
+  cert_port=[]
+  intfLoc1=[]
+  ports=[]
+  @mainremotehash[certname].keys.each do |intf|
+    newremotehash1=@mainremotehash[certname]
+    intfHash = newremotehash1[intf]
+    remote_mac = intfHash["remote_mac"]
+    if !remote_mac
+      remote_mac = intfHash["mac_address"]
+    end
+    intfLoc=intf
+    logger.debug "search_server_Macaddress_force10: intfLoc: #{intfLoc}  mac: #{mac} remote_mac: #{remote_mac.upcase} "
+    if mac.include?(remote_mac)
+      logger.debug "search_server_Macaddress_force10: Matched intfLoc: #{intfLoc}  macArray: #{macArray} remote_mac: #{remote_mac.upcase} "
+      lag=getLAGFromIntf(intf,certname)
+      if !lag.empty?
+        ports=intfLoc1.push(lag)
+      else
+        ports=intfLoc1.push(intfLoc)
+      end
+      interface_found="true"
+    end
+  end
+  if @interface_found == "true"
+    cert_port.push(certname)
+    cert_port.push(ports)
+    @cert_ports.push(cert_port)
+    logger.debug "search_server_Macaddress_force10: Adding in matchedhash remote_mac.: #{remote_mac} and  cert_port: #{cert_port} and cert_ports: #{@cert_ports} "
+    @matchedHash.store(mac.upcase,@cert_ports)
+  end
+
+end
+
+def search_server_Macaddress_powerconnect(certname,mac,logger)
+  interface_found="false"
+  remote_mac=""
+  cert_port=[]
+  intfLoc1=[]
+  ports=[]
+
+  newremotehash1=@mainremotehash[certname]
+  logger.debug " newremotehash1: #{newremotehash1} **************"
+  newremotehash1.keys.each do |remote_mac|
+    intfLoc=newremotehash1[remote_mac]
+    remote_mac=remote_mac.gsub(/(\w{2})(\w{2})\.(\w{2})(\w{2})\.(\w{2})(\w{2})/,'\1:\2:\3:\4:\5:\6')
+    logger.debug "search_server_Macaddress_powerconnect : intfLoc: #{intfLoc}  mac: #{mac} remote_mac: #{remote_mac.upcase} "
+    if mac.include?(remote_mac)
+      logger.debug "search_server_Macaddress_powerconnect : intfLoc: #{intfLoc}   remote_mac: #{remote_mac.upcase} "
+      ports=intfLoc1.push(intfLoc)
+      @interface_found="true"
+    end
+
+  end
+  #end
+  if @interface_found == "true"
+    cert_port.push(certname)
+    cert_port.push(ports)
+    @cert_ports.push(cert_port)
+    logger.debug "search_server_Macaddress_powerconnect: Adding in matchedhash remote_mac.: #{remote_mac} and  cert_port: #{cert_port} and @cert_ports: #{@cert_ports} "
+    @matchedHash.store(mac.upcase,@cert_ports)
+  end
+
+end
+
 public
 
 def search_server_Macaddress(logger)
   serviceTag=@ServiceTag
-
+  #serviceTag=@ServiceTag
   dataHash= @serverinfo
   macArray= dataHash["mac_addresses"]
-  logger.debug "********* macArray :: #{macArray} ********"
-  remote_mac=""
-  macArray.each do |mac|
-    interface_found="false"
-    cert_ports=[]
-    logger.debug "****** mac :: #{mac} *****"
-    logger.debug "****** #{@mainremotehash} *****"
-    @mainremotehash.keys.each do |certname|
-      interface_found="false"
-      cert_port=[]
-      intfLoc1=[]
-      ports=[]
-      @mainremotehash[certname].keys.each do |intf|
-        newremotehash1=@mainremotehash[certname]
-        intfHash = newremotehash1[intf]
-        remote_mac = intfHash["remote_mac"]
-        if !remote_mac
-          remote_mac = intfHash["mac_address"]
-        end
+  #@remote_mac=""
 
-        intfLoc=intf
-        logger.debug "remote_mac :: #{remote_mac}"
-        if mac.include?(remote_mac.upcase)
-          lag=getLAGFromIntf(intf,certname)
-          if !lag.empty?
-          ports=intfLoc1.push(lag)
-          else
-          ports=intfLoc1.push(intfLoc)
-          end
-          interface_found="true"
-        end
-      end
-      if interface_found == "true"
-      cert_port.push(certname)
-      cert_port.push(ports)
-      cert_ports.push(cert_port)
-      @matchedHash.store(mac.upcase,cert_ports)
+  macArray.each do |mac|
+    @interface_found="false"
+    @cert_ports=[]
+    @mainremotehash.keys.each do |certname|
+      match = certname.match(/powerconnect/)
+      if match
+        search_server_Macaddress_powerconnect(certname,mac,logger)
+      else
+        search_server_Macaddress_force10(certname,mac,logger)
       end
     end
   end
   return @matchedHash
 end
+
+public
+
+#def search_server_Macaddress(logger)
+#  serviceTag=@ServiceTag
+#
+#  dataHash= @serverinfo
+#  macArray= dataHash["mac_addresses"]
+#  logger.debug "********* macArray :: #{macArray} ********"
+#  remote_mac=""
+#  macArray.each do |mac|
+#    interface_found="false"
+#    cert_ports=[]
+#    logger.debug "****** mac :: #{mac} *****"
+#    logger.debug "****** #{@mainremotehash} *****"
+#    @mainremotehash.keys.each do |certname|
+#      interface_found="false"
+#      cert_port=[]
+#      intfLoc1=[]
+#      ports=[]
+#      @mainremotehash[certname].keys.each do |intf|
+#        newremotehash1=@mainremotehash[certname]
+#        intfHash = newremotehash1[intf]
+#        remote_mac = intfHash["remote_mac"]
+#        if !remote_mac
+#          remote_mac = intfHash["mac_address"]
+#        end
+#
+#        intfLoc=intf
+#        logger.debug "remote_mac :: #{remote_mac}"
+#        if mac.include?(remote_mac.upcase)
+#          lag=getLAGFromIntf(intf,certname)
+#          if !lag.empty?
+#            ports=intfLoc1.push(lag)
+#          else
+#            ports=intfLoc1.push(intfLoc)
+#          end
+#          interface_found="true"
+#        end
+#      end
+#      if interface_found == "true"
+#        cert_port.push(certname)
+#        cert_port.push(ports)
+#        cert_ports.push(cert_port)
+#        @matchedHash.store(mac.upcase,cert_ports)
+#      end
+#    end
+#  end
+#  return @matchedHash
+#end
 
 public
 
@@ -112,7 +203,7 @@ def getLAGFromIntf(intf,certname)
       end
     end
     if ports.include?(intf)
-    return lag
+      return lag
     end
   end
   return ""
