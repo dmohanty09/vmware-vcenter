d78eaa89708c8216b6481c5839237bfa17183504
Gavin Scott <gavin_scott@dell.com>
2014-05-21 21:33:28 -0500

    update ASM::NetworkConfiguration for rack server data
    Rack server nic data is passed in an 'interfaces' array and blade
    server nic data is passed in a 'fabric' array. NetworkConfiguration
    creates a new field called 'cards' that contains the same nic data for
    ease of parsing. It also strips out all disabled, non-applicable and
    FC cards, ports and partitions for ease of parsing. The original data
    is still available in the 'fabrics' or 'interfaces' array.
    
    Additionally rack nic slots are mapped to interfaces in the order they
    appear. E.g. if a rack server has nics in slots 3, 5 and 7 and the
    configuration data specifies two cards, slots 3 and 5 will be used.

--- lib/asm/network_configuration.rb
+++ lib/asm/network_configuration.rb
@@ -1,45 +1,59 @@
+require 'asm/errors'
 require 'asm/wsman'
 require 'hashie'
 
 module ASM
 
-  class NetworkConfiguration < Hashie::Mash
+  class NetworkConfiguration
 
-    class NicInfo < Hashie::Mash
+    attr_accessor(:logger)
+
+    def initialize(network_config_hash, logger = nil)
+      @mash = Hashie::Mash.new(network_config_hash)
+      @logger = logger
+      self.munge!
+    end
+
+    # Forward methods we don't define directly to the mash
+    def method_missing(sym, *args, &block)
+      @mash.send(sym, *args, &block)
+    end
+
+    class NicInfo
 
       def initialize(fqdd, logger = nil)
-        parse_fqdd!(fqdd, logger)
+        @mash = parse_fqdd(fqdd, logger)
+      end
+
+      # Forward methods we don't define directly to the mash
+      def method_missing(sym, *args, &block)
+        @mash.send(sym, *args, &block)
       end
 
       def card_to_fabric(card)
         ['A', 'B', 'C'][card.to_i - 1]
       end
 
-      def parse_fqdd!(fqdd, logger)
+      def parse_fqdd(fqdd, logger)
+        ret = Hashie::Mash.new
         # Expected format: NIC.Mezzanine.2B-1-1
-        self.fqdd = fqdd
-        (_, self.type, port_info) = self.fqdd.split('.')
-        (self.card, self.port, self.partition_no) = port_info.split('-')
-        if self.card =~ /([0-9])([A-Z])/
-          orig_card = self.card
-          self.card = $1
-          self.fabric = $2
+        ret.fqdd = fqdd
+        (_, ret.type, port_info) = ret.fqdd.split('.')
+        (ret.card, ret.port, ret.partition_no) = port_info.split('-')
+        if ret.card =~ /([0-9])([A-Z])/
+          orig_card = ret.card
+          ret.card = $1
+          ret.fabric = $2
           expected_fabric = card_to_fabric(orig_card)
-          if self.fabric != expected_fabric
-            logger.warn("Mismatched fabric information for #{orig_card}: #{self.fabric} versus #{expected_fabric}")
+          if ret.fabric != expected_fabric
+            logger.warn("Mismatched fabric information for #{orig_card}: #{ret.fabric} versus #{expected_fabric}")
           end
         else
-          self.fabric = card_to_fabric(self.card)
+          ret.fabric = card_to_fabric(ret.card)
         end
+        ret
       end
 
-    end
-
-    attr_accessor(:logger)
-
-    def initialize(network_config_hash, logger = nil)
-      super.initialize(network_config_hash)
-      @logger = logger
     end
 
     def get_wsman_nic_info(endpoint)
@@ -61,7 +75,7 @@ module ASM
 
     def name_to_port(port_name)
       if port_name =~ /Port ([0-9]*)/
-        $1
+        $1.to_i
       else
         raise(Exception, "Invalid port name #{port_name}")
       end
@@ -69,14 +83,14 @@ module ASM
 
     def name_to_partition(partition_name)
       if partition_name =~ /([0-9]*)/
-        $1
+        $1.to_i
       else
         raise(Exception, "Invalid partition name #{partition_name}")
       end
     end
 
     def get_partitions(*network_types)
-      self.fabrics.collect do |fabric|
+      @mash.cards.collect do |fabric|
         fabric.interfaces.collect do |port|
           port.partitions.find_all do |partition|
             partition.networkObjects && partition.networkObjects.find do |network|
@@ -87,49 +101,149 @@ module ASM
       end.flatten
     end
 
-    def add_partition_info!
-      # Augment partitions with additional info
-      partition_i = 0
-      self.fabrics.each do |fabric|
-        if ASM::Util.to_boolean(fabric.enabled)
-          fabric.interfaces.each do |port|
-            port.partitions.each do |partition|
-              partition.fabric_letter = name_to_fabric(fabric.name)
-              partition.port_no = name_to_port(port.name)
-              partition.partition_no = name_to_partition(partition.name)
-              partition.partition_index = partition_i
-              partition_i += 1
+    # Finds all networks of one of the specified network types
+    def get_networks(*network_types)
+      @mash.cards.collect do |fabric|
+        fabric.interfaces.collect do |port|
+          port.partitions.collect do |partition|
+            partition.networkObjects.find_all do |network|
+              network_types.include?(network.type)
             end
           end
+        end
+      end.flatten.uniq
+    end
+
+    # Returns the network object for the given network type.  This method raises
+    # an exception if more than one network is found, so it is never valid to
+    # call it for network types that may have more than one network associated
+    # with them such as iSCSI or public/private lan.
+    def get_network(network_type)
+      ret = get_networks(network_type)
+      if ret.size == 1
+        ret[0]
+      else
+        raise(Exception, "There should be only one #{network_type} network but found #{ret.size}: #{ret.collect { |n| n.name }}")
+      end
+    end
+
+    def is_blade?
+      @mash.servertype == 'blade'
+    end
+
+    def is_rack?
+      @mash.servertype == 'rack'
+    end
+
+    def munge!
+      # Augment partitions with additional info
+      source = case @mash.servertype
+                 when 'blade'
+                   @mash.interfaces = nil
+                   @mash.fabrics
+                 when 'rack'
+                   @mash.fabrics = nil
+                   @mash.interfaces
+                 else
+                   raise(Exception, "Unsupported server type in network configuration: #{@mash.servertype}")
+               end
+
+      partition_i = 0
+      interface_i = 0
+      card_i = 0
+      @mash.cards = []
+      source.each do |orig_card|
+        # For now we are discarding FC interfaces!
+        if ASM::Util.to_boolean(orig_card.enabled) && !ASM::Util.to_boolean(orig_card.usedforfc)
+          card = Hashie::Mash.new(orig_card)
+          card.interfaces = []
+          orig_card.interfaces.each do |orig_interface|
+            interface = Hashie::Mash.new(orig_interface)
+            interface.partitions = []
+            port_no = name_to_port(orig_interface.name).to_i
+            nic_type = card.nictype.to_i
+            max_partitions = case nic_type
+                               when 4
+                                 # Quad-port nics can have two partitions
+                                 2
+                               when 2
+                                 # Dual-port nics can have 4
+                                 4
+                               else
+                                 # Defaulting to 4...
+                                 logger.warn("Unsupported nic type: #{nic_type}") if logger
+                                 4
+                             end
+            if nic_type >= port_no
+              orig_interface.interface_index = interface_i
+              interface_i += 1
+              orig_interface.partitions.each do |partition|
+                partition_no = name_to_partition(partition.name)
+                if partition_no == 1 || (interface.partitioned && partition_no <= max_partitions)
+                  if is_blade?
+                    partition.fabric_letter = name_to_fabric(card.name)
+                  end
+                  partition.port_no = port_no
+                  partition.partition_no = partition_no
+                  partition.partition_index = partition_i
+                  partition_i += 1
+
+                  interface.partitions.push(partition)
+                end
+              end
+              card.interfaces.push(interface)
+            end
+          end
+          card.card_index = card_i
+          card_i += 1
+          @mash.cards.push(card)
         end
       end
     end
 
+    def build_index_to_slot_hash(nics)
+      slots = nics.collect { |nic| nic.card.to_i }.compact.sort.uniq
+
+      unless slots.size >= @mash.cards.size
+        fqdds = nics.collect { |nic| nic.fqdd }
+        logger.debug("Found nic fqdd's: #{fqdds}") if logger
+        raise(ASM::UserException, "Network configuration requires #{@mash.cards.size} network cards but only #{slots.size} were found")
+      end
+
+      ret = {}
+      @mash.cards.collect { |card| card.card_index }.each do |i|
+        ret[i] = slots[i]
+      end
+      ret
+    end
+
     def add_nics!(endpoint)
       nics = get_wsman_nic_info(endpoint)
-      self.fabrics.each do |fabric|
-        fabric.interfaces.each do |port|
-          port.partitions.each do |partition|
-            # HACK: It is currently impossible to tell which partitions the user
-            # has selected versus those that were not visible to the user. For
-            # now we only tread partitions with networks as "selected"
-            if partition.networkObjects && !partition.networkObjects.empty?
-              if partition.name == '1' || port.partitioned
-                nic = nics.find do |n|
-                  (name_to_fabric(fabric.name) == n.fabric &&
-                      name_to_port(port.name) == n.port &&
-                      name_to_partition(partition.name) == n.partition_no)
-                end
 
-                if nic
-                  partition.nic = nic
-                  partition.fqdd = nic.fqdd
-                  partition.mac_address = nic.mac_address
-                else
-                  msg = "Mac address not found on #{endpoint.host} for #{fabric.name} #{port.name} partition #{partition.name}"
-                  raise(Exception, msg)
-                end
+      @mash.cards.each do |card|
+        card.interfaces.each do |interface|
+          interface.partitions.each do |partition|
+            nic = nics.find do |n|
+              if is_blade?
+                (name_to_fabric(card.name) == n.fabric &&
+                    name_to_port(interface.name).to_s == n.port &&
+                    name_to_partition(partition.name).to_s == n.partition_no)
+              else
+                slot_map ||= build_index_to_slot_hash(nics)
+                slot = slot_map[card.card_index] or raise(Exception, "No slot found for card_index #{card.card_index} in #{slot_map}")
+                (slot.to_s == n.card &&
+                    name_to_port(interface.name).to_s == n.port &&
+                    name_to_partition(partition.name).to_s == n.partition_no)
               end
+            end
+
+            if nic
+              partition.nic = nic
+              partition.fqdd = nic.fqdd
+              partition.mac_address = nic.mac_address
+            else
+              msg = "Mac address not found on #{endpoint.host} for #{card.name} #{interface.name} partition #{partition.name}"
+              raise(Exception, msg)
             end
           end
         end
--- spec/unit/asm/network_configuration_spec.rb
+++ spec/unit/asm/network_configuration_spec.rb
@@ -5,6 +5,8 @@ describe ASM::NetworkConfiguration do
 
   describe 'when parsing NIC FQDDs' do
 
+    # Blade examples:
+    #
     # NIC.Integrated.1-1-1: 24:B6:FD:F9:FC:42
     # NIC.Integrated.1-1-2: 24:B6:FD:F9:FC:46
     # NIC.Integrated.1-1-3: 24:B6:FD:F9:FC:4A
@@ -21,6 +23,17 @@ describe ASM::NetworkConfiguration do
     # NIC.Mezzanine.2B-2-2: 00:10:18:DC:C4:86
     # NIC.Mezzanine.2B-2-3: 00:10:18:DC:C4:8A
     # NIC.Mezzanine.2B-2-4: 00:10:18:DC:C4:8E
+    #
+    # Rack exmaples:
+    #
+    # NIC.Slot.2-1-1: 00:0A:F7:06:88:50
+    # NIC.Slot.2-1-2: 00:0A:F7:06:88:54
+    # NIC.Slot.2-1-3: 00:0A:F7:06:88:58
+    # NIC.Slot.2-1-4: 00:0A:F7:06:88:5C
+    # NIC.Slot.2-2-1: 00:0A:F7:06:88:52
+    # NIC.Slot.2-2-2: 00:0A:F7:06:88:56
+    # NIC.Slot.2-2-3: 00:0A:F7:06:88:5A
+    # NIC.Slot.2-2-4: 00:0A:F7:06:88:5E
 
     it 'should parse NIC.Integrated.1-1-1' do
       fqdd = ASM::NetworkConfiguration::NicInfo.new('NIC.Integrated.1-1-1')
@@ -60,13 +73,33 @@ describe ASM::NetworkConfiguration do
       fqdd.partition_no.should == '4'
     end
 
+    it 'should parse rack fqdd in port 1' do
+      fqdd = ASM::NetworkConfiguration::NicInfo.new('NIC.Slot.2-1-1')
+      fqdd.type.should == 'Slot'
+      fqdd.card.should == '2'
+      # NOTE: these are rack fqdds, so maybe we should not populate fabric info...
+      fqdd.fabric.should == 'B'
+      fqdd.port.should == '1'
+      fqdd.partition_no.should == '1'
+    end
+
+    it 'should parse rack fqdd in port 2' do
+      fqdd = ASM::NetworkConfiguration::NicInfo.new('NIC.Slot.2-2-3')
+      fqdd.type.should == 'Slot'
+      fqdd.card.should == '2'
+      # NOTE: these are rack fqdds, so maybe we should not populate fabric info...
+      fqdd.fabric.should == 'B'
+      fqdd.port.should == '2'
+      fqdd.partition_no.should == '3'
+    end
+
   end
 
   describe 'when parsing a partitioned network config' do
 
     before do
       file_name = File.join(File.dirname(__FILE__), '..', '..',
-                            'fixtures', 'network_configuration', 'partitioned.json')
+                            'fixtures', 'network_configuration', 'blade_partitioned.json')
       @data = JSON.parse(File.read(file_name))
     end
 
@@ -83,8 +116,8 @@ describe ASM::NetworkConfiguration do
             if fabric_letter == 'A'
               # In sample data, Fabric A is enabled
               partition.fabric_letter.should == fabric_letter
-              partition.port_no.should == port_no.to_s
-              partition.partition_no.should == partition_no.to_s
+              partition.port_no.should == port_no
+              partition.partition_no.should == partition_no
               partition.partition_index.should == partition_index
               partition_index += 1
             else
@@ -163,6 +196,47 @@ describe ASM::NetworkConfiguration do
       partitions[1].name.should == '3'
     end
 
+    it 'should find single networks' do
+      networks = ASM::NetworkConfiguration.new(@data).get_networks('HYPERVISOR_MANAGEMENT')
+      networks.size.should == 1
+      network = networks[0]
+      network.name.should == 'Hypervisor Management'
+      network.staticNetworkConfiguration.ipAddress.should == '172.28.12.118'
+    end
+
+    it 'should not find missing networks' do
+      networks = ASM::NetworkConfiguration.new(@data).get_networks('FILESHARE')
+      networks.size.should == 0
+    end
+
+    it 'should find multiple networks' do
+      networks = ASM::NetworkConfiguration.new(@data).get_networks('PXE', 'HYPERVISOR_MANAGEMENT')
+      networks.size.should == 2
+    end
+
+    it 'should find single network type' do
+      network = ASM::NetworkConfiguration.new(@data).get_network('HYPERVISOR_MANAGEMENT')
+      network.name.should == 'Hypervisor Management'
+      network.staticNetworkConfiguration.ipAddress.should == '172.28.12.118'
+    end
+
+    it 'should fail to find single network if multiple management networks found' do
+      network_config = ASM::NetworkConfiguration.new(@data)
+      orig = network_config.get_network('HYPERVISOR_MANAGEMENT')
+      dup = Hashie::Mash.new(orig)
+      dup.staticNetworkConfiguration.ipAddress = '172.28.12.119'
+      network_config.cards[0].interfaces[0].partitions[2].networkObjects.push(dup)
+      expect do
+        network = network_config.get_network('HYPERVISOR_MANAGEMENT')
+      end.to raise_error(Exception)
+    end
+
+    it 'should fail to find single network if multiple management networks found' do
+      expect do
+        network = ASM::NetworkConfiguration.new(@data).get_network('STORAGE_ISCSI_SAN')
+      end.to raise_error(Exception)
+    end
+
     it 'should find storage networks with correct mac addresses' do
       fqdd_to_mac = {'NIC.Integrated.1-1-1' => '00:0E:1E:0D:8C:30',
                      'NIC.Integrated.1-1-2' => '00:0E:1E:0D:8C:32',
@@ -192,7 +266,7 @@ describe ASM::NetworkConfiguration do
 
     before do
       file_name = File.join(File.dirname(__FILE__), '..', '..',
-                            'fixtures', 'network_configuration', 'unpartitioned.json')
+                            'fixtures', 'network_configuration', 'blade_unpartitioned.json')
       @data = JSON.parse(File.read(file_name))
     end
 
@@ -207,7 +281,6 @@ describe ASM::NetworkConfiguration do
       ASM::WsMan.stubs(:get_mac_addresses).returns(fqdd_to_mac)
       net_config = ASM::NetworkConfiguration.new(@data)
       net_config.add_nics!(Hashie::Mash.new({:host => '127.0.0.1'}))
-      fabric = net_config.fabrics.find { |fabric| fabric.name == 'Fabric A' }
 
       # Verify all partition 1 set correctly
       ('A'..'C').each_with_index do |fabric_letter, fabric_index|
@@ -227,6 +300,114 @@ describe ASM::NetworkConfiguration do
             else
               partition.fqdd.should be_nil
               partition.mac_address.should be_nil
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe 'when parsing a partitioned rack network config' do
+
+    before do
+      file_name = File.join(File.dirname(__FILE__), '..', '..',
+                            'fixtures', 'network_configuration', 'rack_partitioned.json')
+      @data = JSON.parse(File.read(file_name))
+    end
+
+    it 'should match first card to first interface' do
+      fqdd_to_mac = {'NIC.Slot.2-1-1' => '00:0A:F7:06:88:50',
+                     'NIC.Slot.2-1-2' => '00:0A:F7:06:88:54',
+                     'NIC.Slot.2-1-3' => '00:0A:F7:06:88:58',
+                     'NIC.Slot.2-1-4' => '00:0A:F7:06:88:5C',
+                     'NIC.Slot.2-2-1' => '00:0A:F7:06:88:52',
+                     'NIC.Slot.2-2-2' => '00:0A:F7:06:88:56',
+                     'NIC.Slot.2-2-3' => '00:0A:F7:06:88:5A',
+                     'NIC.Slot.2-2-4' => '00:0A:F7:06:88:5E'}
+
+      ASM::WsMan.stubs(:get_mac_addresses).returns(fqdd_to_mac)
+      net_config = ASM::NetworkConfiguration.new(@data)
+      net_config.add_nics!(Hashie::Mash.new({:host => '127.0.0.1'}))
+
+      # Verify
+      slot1 = net_config.interfaces[0]
+      (1..2).each do |port_no|
+        port = slot1.interfaces.find { |p| p.name == "Port #{port_no}" }
+        (1..4).each do |partition_no|
+          fqdd = "NIC.Slot.2-#{port_no}-#{partition_no}"
+          partition = port.partitions.find { |p| p.name == partition_no.to_s }
+          partition.fqdd.should == fqdd
+          partition.mac_address.should == fqdd_to_mac[fqdd]
+        end
+      end
+    end
+  end
+
+  describe 'when parsing an un-partitioned rack network config' do
+
+    before do
+      file_name = File.join(File.dirname(__FILE__), '..', '..',
+                            'fixtures', 'network_configuration', 'rack_unpartitioned.json')
+      @data = JSON.parse(File.read(file_name))
+    end
+
+    it 'should match first cards to first interfaces' do
+      fqdd_to_mac = {'NIC.Slot.5-1-1' => '00:0A:F7:06:88:50',
+                     'NIC.Slot.5-1-2' => '00:0A:F7:06:88:54',
+                     'NIC.Slot.5-1-3' => '00:0A:F7:06:88:58',
+                     'NIC.Slot.5-1-4' => '00:0A:F7:06:88:5C',
+                     'NIC.Slot.5-2-1' => '00:0A:F7:06:88:52',
+                     'NIC.Slot.5-2-2' => '00:0A:F7:06:88:56',
+                     'NIC.Slot.5-2-3' => '00:0A:F7:06:88:5A',
+                     'NIC.Slot.5-2-4' => '00:0A:F7:06:88:5E',
+                     'NIC.Slot.3-1-1' => '01:0A:F7:06:88:50',
+                     'NIC.Slot.3-1-2' => '01:0A:F7:06:88:54',
+                     'NIC.Slot.3-1-3' => '01:0A:F7:06:88:58',
+                     'NIC.Slot.3-1-4' => '01:0A:F7:06:88:5C',
+                     'NIC.Slot.3-2-1' => '01:0A:F7:06:88:52',
+                     'NIC.Slot.3-2-2' => '01:0A:F7:06:88:56',
+                     'NIC.Slot.3-2-3' => '01:0A:F7:06:88:5A',
+                     'NIC.Slot.3-2-4' => '01:0A:F7:06:88:5E',
+                     'NIC.Slot.1-1-1' => '02:0A:F7:06:88:50',
+                     'NIC.Slot.1-1-2' => '02:0A:F7:06:88:54',
+                     'NIC.Slot.1-1-3' => '02:0A:F7:06:88:58',
+                     'NIC.Slot.1-1-4' => '02:0A:F7:06:88:5C',
+                     'NIC.Slot.1-2-1' => '02:0A:F7:06:88:52',
+                     'NIC.Slot.1-2-2' => '02:0A:F7:06:88:56',
+                     'NIC.Slot.1-2-3' => '02:0A:F7:06:88:5A',
+                     'NIC.Slot.1-2-4' => '02:0A:F7:06:88:5E',
+                     'NIC.Slot.7-1-1' => '03:0A:F7:06:88:50',
+                     'NIC.Slot.7-1-2' => '03:0A:F7:06:88:54',
+                     'NIC.Slot.7-1-3' => '03:0A:F7:06:88:58',
+                     'NIC.Slot.7-1-4' => '03:0A:F7:06:88:5C',
+                     'NIC.Slot.7-2-1' => '03:0A:F7:06:88:52',
+                     'NIC.Slot.7-2-2' => '03:0A:F7:06:88:56',
+                     'NIC.Slot.7-2-3' => '03:0A:F7:06:88:5A',
+                     'NIC.Slot.7-2-4' => '03:0A:F7:06:88:5E'}
+
+      ASM::WsMan.stubs(:get_mac_addresses).returns(fqdd_to_mac)
+      net_config = ASM::NetworkConfiguration.new(@data)
+      net_config.add_nics!(Hashie::Mash.new({:host => '127.0.0.1'}))
+
+      # Verify 3 cards, unpartitioned
+      to_slots = {0 => 1, 1 => 3, 2 => 5, 3 => 7}
+      found_macs = []
+      (0..3).each do |card_index|
+        card = net_config.cards.find { |c| c.card_index == card_index }
+        slot = to_slots[card_index]
+        (1..2).each do |port_no|
+          port = card.interfaces.find { |p| p.name == "Port #{port_no}" }
+          (1..4).each do |partition_no|
+            fqdd = "NIC.Slot.#{slot}-#{port_no}-#{partition_no}"
+            partition = port.partitions.find { |p| p.name == partition_no.to_s }
+            if partition_no > 1
+              partition.should be_nil
+            else
+              partition.fqdd.should == fqdd
+              mac = fqdd_to_mac[fqdd]
+              partition.mac_address.should == mac
+              found_macs.include?(mac).should be_false
+              found_macs.push(mac)
             end
           end
         end
