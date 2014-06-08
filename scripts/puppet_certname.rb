d3c8b4e2c34e067a7559eec3157963895b2654b7
Nan Liu <nan.liu@gmail.com>
2014-06-08 00:41:13 -0700

    Ensure macaddress downcase in certname.

--- scripts/puppet_certname.rb
+++ scripts/puppet_certname.rb
@@ -8,5 +8,6 @@ config = IniFile.load(Puppet[:config])
 config['agent'] ||= {}
 macaddress = Facter.value('macaddress')
 raise(Exception, "Can not detect system macaddress.") unless macaddress
-config['agent']['certname'] = "vm#{macaddress.gsub(':','')}"
+config['main']['certname'] = "vm#{macaddress.gsub(':','').downcase}"
+config['agent']['certname'] = "vm#{macaddress.gsub(':','').downcase}"
 config.save
