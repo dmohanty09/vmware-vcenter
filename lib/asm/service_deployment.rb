fa835aae1961927bceca7c24029917476e4ee15e
Charles Paul <charles.paul@gmail.com>
2014-06-09 14:53:40 -0500

    no longer configure mem

--- lib/asm/service_deployment.rb
+++ lib/asm/service_deployment.rb
@@ -1752,7 +1752,7 @@ class ASM::ServiceDeployment
                         'require'                => [
                           "Esx_datastore[#{hostip}:#{storage_title}]",
                           "Esx_syslog[#{hostip}]"],
-                        'configure_mem'          => true,
+                        'configure_mem'          => false,
                         'install_mem'            => true,
                         'script_executable_path' => '/opt/Dell/scripts/EquallogicMEM',
                         'setup_script_filepath'  => 'setup.pl',
